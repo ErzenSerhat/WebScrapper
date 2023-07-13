9349ce4ff241a4f3402bf7fe013ab83a0191452d
Serhat <100749817+ErzenSerhat@users.noreply.github.com>
2023-07-13 04:49:46 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const PORT = 5000
+const url ='https://www.haberler.com'
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+
+
+axios(url)
+    .then(response => {
+        try {
+            const html = response.data
+            const data = cheerio.load(html)
+
+            const news = []
+
+            data('.p12-col', html).each(function(){ // element class name
+            const title = data(this).find('.hbBoxText').text()
+            const imageUrl = data(this).find('.hbBoxImage').attr('data-src')
+            news.push({
+                title,imageUrl
+                })
+            })
+            console.log(news)
+            }catch (error) {
+            console.log(error)
+        }
+    })
+
+app.listen(PORT, () => console.log('server runing'))
